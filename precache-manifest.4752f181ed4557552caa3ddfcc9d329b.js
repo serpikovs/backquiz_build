959d728eff558845a046743d6b545a6fee10eca6
Sergey Serpikov <iam32five@gmail.com>
2020-04-13 20:13:32 +0300

    Initial commit

---
+++ precache-manifest.4752f181ed4557552caa3ddfcc9d329b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b06acd5901c878bc442e03ba16f8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e31a02fc8e2e0cf1c709",
+    "url": "/static/css/main.9a585b6a.chunk.css"
+  },
+  {
+    "revision": "e61f76469bbe8828df21",
+    "url": "/static/js/2.ffe1c6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffe1c6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31a02fc8e2e0cf1c709",
+    "url": "/static/js/main.4e2bba00.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
